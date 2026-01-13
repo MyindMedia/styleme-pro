f11d61ddb07b8bf5c699c9a0e28d965b1ec30a1a
MyindMedia <info@myindmedia.org>
2026-01-13 12:06:32 -0800

    Fix Netlify build: Add postinstall script and update metro config for react-native-css-interop

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Make sure metro is watching the package cache folder
+config.watchFolders = config.watchFolders || [];
+config.watchFolders.push(
+  path.resolve(__dirname, "node_modules", "react-native-css-interop", ".cache"),
+);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
